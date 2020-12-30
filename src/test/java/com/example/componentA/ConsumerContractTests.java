2da7bd2560af92a3e80d25f329196fa758092156
Nicola Tuveri <nicola.tuveri@inpeco.com>
2020-12-30 19:42:34 +0100

    add server error interaction

--- src/test/java/com/example/componentA/ConsumerContractTests.java
+++ src/test/java/com/example/componentA/ConsumerContractTests.java
@@ -10,6 +10,7 @@ import au.com.dius.pact.consumer.junit.PactVerification;
 import au.com.dius.pact.consumer.model.MockProviderConfig;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -68,6 +69,7 @@ public class ConsumerContractTests {
 			this.provider.getUrl() + "/alligators/Mary", HttpMethod.GET, request, String.class);
 
 		Assert.assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+		Assert.assertEquals("Mary", JsonPath.read(response.getBody(), "$.name"));
 	}
 
 	@Pact(provider = PROVIDER_NAME, consumer = CONSUMER_NAME)
@@ -99,5 +101,38 @@ public class ConsumerContractTests {
 			this.provider.getUrl() + "/alligators/Mary", HttpMethod.GET, request, String.class);
 
 		Assert.assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCodeValue());
+	}
+
+	@Pact(provider = PROVIDER_NAME, consumer = CONSUMER_NAME)
+	public RequestResponsePact getAlligatorMaryError(PactDslWithProvider builder) {
+
+		RequestResponsePact responsePact = builder
+			.given("an error occurs retrieving an alligator")
+			.uponReceiving("a request for an alligator")
+			.path("/alligators/Mary")
+			.method(HttpMethod.GET.name())
+			.headers(Collections.singletonMap(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON.toString()))
+			.willRespondWith()
+			.headers(Collections.singletonMap(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString()))
+			.status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+			.body(new PactDslJsonBody().stringValue("error", "Argh!"))
+			.toPact();
+
+		return responsePact;
+	}
+
+	@Test
+	@PactVerification(value = PROVIDER_NAME, fragment = "getAlligatorMaryError")
+	public void givenAnErrorOccurs_WhenAlligatorMaryIsRequested_ThenResponseIsServerError() {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> request = new HttpEntity<>(null, headers);
+
+		ResponseEntity<String> response = this.consumer.exchange(
+			this.provider.getUrl() + "/alligators/Mary", HttpMethod.GET, request, String.class);
+
+		Assert.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), response.getStatusCodeValue());
+		Assert.assertEquals("Argh!", JsonPath.read(response.getBody(), "$.error"));
 	}
 }
